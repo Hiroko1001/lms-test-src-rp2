1a28b96e51c6acf425655ac6ad09918059e854e0
h-hayashi <h.hayashi@drivingforce.co.jp>
2025-11-27 10:09:15 +0900

    add:Case01.java トップページもといログイン画面への遷移テストコード追加 Case02.java トップページもといログイン画面への遷移テストコード、またログインエラーメッセージが表示されるテストコード追加

---
+++ test_code_creation_hayashihiroko/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -0,0 +1,64 @@
+package jp.co.sss.lms.ct.f01_login1;
+
+import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+/**
+ * 結合テスト ログイン機能①
+ * ケース01
+ * @author holy
+ */
+@TestMethodOrder(OrderAnnotation.class)
+@DisplayName("ケース01 ログイン画面への遷移")
+public class Case01 {
+
+
+	/** 前処理 */
+	@BeforeAll
+	static void before() {
+
+		System.setProperty("webdriver.chrome.driver", "lib\\chromedriver.exe");
+		createDriver();
+	}
+
+	/** 後処理 */
+	@AfterAll
+	static void after() {
+		
+		System.setProperty("webdriver.chrome.driver", "lib\\chromedriver.exe");
+		closeDriver();
+
+	}
+
+	@Test
+	@Order(1)
+	@DisplayName("テスト01 トップページURLでアクセス")
+	void test01() {
+		// トップページの画面を開く
+		goTo("http://localhost:8080/lms");
+
+		// 各画面表示時に10秒待機する(暗黙的)
+		webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
+		
+		//ログイン画面が出てるかどうかを確認、比較
+		final WebElement title = webDriver.findElement(By.xpath("//*[@id=\"main\"]/h2"));
+		assertEquals(title.getText(), "ログイン");
+	}
+
+}
---
+++ test_code_creation_hayashihiroko/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -0,0 +1,86 @@
+package jp.co.sss.lms.ct.f01_login1;
+
+import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+/**
+ * 結合テスト ログイン機能①
+ * ケース02
+ * @author holy
+ */
+@TestMethodOrder(OrderAnnotation.class)
+@DisplayName("ケース02 受講生 ログイン 認証失敗")
+public class Case02 {
+
+	/** 前処理 */
+	@BeforeAll
+	static void before() {
+
+		System.setProperty("webdriver.chrome.driver", "lib\\chromedriver.exe");
+		createDriver();
+	}
+
+	/** 後処理 */
+	@AfterAll
+	static void after() {
+
+		System.setProperty("webdriver.chrome.driver", "lib\\chromedriver.exe");
+		closeDriver();
+	}
+
+	@Test
+	@Order(1)
+	@DisplayName("テスト01 トップページURLでアクセス")
+	void test01() {
+		// トップページの画面を開く
+		goTo("http://localhost:8080/lms");
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
+
+		//ログイン画面のタイトルを比較
+		final WebElement title = webDriver.findElement(By.xpath("//*[@id=\"main\"]/h2"));
+		assertEquals(title.getText(), "ログイン");
+	}
+
+	@Test
+	@Order(2)
+	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
+	void test02() {
+		// エラーメッセージが表示されることを確認
+
+		//ログインIDにDBに登録されてない「a」を入力
+		webDriver.findElement(By.name("loginId")).sendKeys("a");
+		//パスワードにDBに登録されてない「a」を入力
+		webDriver.findElement(By.name("password")).sendKeys("a");
+
+		//ログインボタンを押下する
+		webDriver.findElement(By.className("btn-primary")).click();
+		
+		// 各画面表示時に10秒待機する(暗黙的)
+		webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		//スクリーンショットを撮影
+		getEvidence(new Object() {
+		});
+
+		//エラーメッセージが出てるかどうか確認、比較
+		final WebElement errorMsg = webDriver.findElement(By.className("error"));
+		assertEquals(errorMsg.getText(), "* ログインに失敗しました。");
+
+	}
+
+}
